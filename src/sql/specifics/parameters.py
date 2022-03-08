c688efa86e53b304a0f4d94b07652093ed956fbe
Sulabh Shrestha <sulabh.shr@gmail.com>
2022-03-08 15:23:48 -0500

    Remove specifics from src

--- src/sql/specifics/parameters.py
+++
@@ -1,4 +0,0 @@
-ordered_view_names = ['shippedVsCustDemand', 'totalManufItems', 'matsUsedVsShipped',
-                      'producedVsShipped', 'suppliedVsShipped', 'perSupplierCost', 'perManufCost',
-                      'perShipperCost', 'totalCostBreakDown', 'customersWithUnsatisfiedDemand',
-                      'suppliersWithUnsentOrders', 'manufsWoutEnoughMats', 'manufsWithUnsentOrders']
